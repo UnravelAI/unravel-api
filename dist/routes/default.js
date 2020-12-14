956a901b09debe9e3b7b5ea7b8ecd42abeef4488
AlyMBarakat <aly.m.barakat@gmail.com>
2020-12-14 02:14:27 +0200

    Create default router for controllers routing

---
+++ dist/routes/default.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+// import controllers
+// for example:
+// import Users from "../controllers/users";
+// route controllers
+// for example:
+// router.use("/users", Users);
+exports.default = router;
+//# sourceMappingURL=default.js.map
---
+++ src/routes/default.ts
@@ -0,0 +1,16 @@
+import Express from "express";
+const router = Express.Router();
+
+
+// import controllers
+// for example:
+// import Users from "../controllers/users";
+
+
+
+// route controllers
+// for example:
+// router.use("/users", Users);
+
+
+export default router;
