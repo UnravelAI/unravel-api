5f350997ad763d73e4fbb278d1e500adb3e382e3
AlyMBarakat <aly.m.barakat@gmail.com>
2020-12-31 17:05:53 +0300

    Create users controller

---
+++ src/controllers/users.ts
@@ -0,0 +1,75 @@
+import Express from "express";
+import { getConnection, Repository } from "typeorm";
+import { User } from "../entity/user";
+import { checkIfExists, validateUserFields, UniqueUserElements } from "../helpers/users"
+
+const router = Express.Router();
+
+
+// create new user
+router.post("/", async (req: Express.Request, res: Express.Response) => {
+    try {
+        const user: User = req.body;
+        const validationErrors: string[] = validateUserFields(user);
+        const uniqueElements: UniqueUserElements = {
+            email: user.email,
+            passwordSalt: user.passwordSalt,
+        }
+        if (validationErrors.length !== 0) {
+            return res.status(400).json({
+                message: "Validation errors occured",
+                errors: validationErrors,
+            });
+        }
+        const uniqueElementsErrors: string[] = await checkIfExists(uniqueElements);
+        if (uniqueElementsErrors.length !== 0) {
+            return res.status(400).json({
+                message: "Unique errors occured",
+                errors: uniqueElementsErrors,
+            });
+        }
+        const userRepository: Repository<User> = await getConnection().getRepository(User);
+        const newUser: User = await userRepository.save(user);
+        return res.status(201).json({
+            message: "User created successfully",
+            data: newUser,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
+// read all users
+router.get("/", async (req: Express.Request, res: Express.Response) => {
+    try {
+        const userRepository: Repository<User> = await getConnection().getRepository(User);
+        const users: User[] = await userRepository.find()
+        return res.status(201).json({
+            message: "Users Retreived successfully",
+            data: users,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
+// read all users
+router.delete("/:id", async (req: Express.Request, res: Express.Response) => {
+    try {
+        const userRepository: Repository<User> = await getConnection().getRepository(User);
+        await userRepository.delete(req.params.id);
+        return res.status(201).json({
+            message: "User deleted successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
+export default router;
