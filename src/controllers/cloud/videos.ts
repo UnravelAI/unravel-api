5748144a6512864dd44c2e698006112b15b138dc
AlyMBarakat <aly.m.barakat@gmail.com>
2021-03-31 03:35:10 +0200

    Add jobStatus endpoint

--- src/controllers/cloud/videos.ts
+++ src/controllers/cloud/videos.ts
@@ -1,6 +1,7 @@
 import Express from "express";
 import { Request, Response } from "express";
 import { getConnection, Repository } from "typeorm";
+import { Video } from "../../entity/video";
 
 const router = Express.Router();
 
@@ -12,10 +13,24 @@ const router = Express.Router();
 */
 
 
-/*
+// Set video upload job status
+router.put("/jobStatus", async (req: Request, res: Response) => {
+    try {
+        const fileName: string = req.body.fileName;
 
-    Job completed endpoint
+        // update jobStatus for the specified file
+        const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
+        await videoRepository.update({ fileName }, { jobCompleted: true });
 
-*/
+        return res.status(200).json({
+            message: `jobStatus updated successfully for video: ${fileName}`,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+});
 
 export default router;
