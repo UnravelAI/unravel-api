16897292efdbf8c4f85ac84fd824b14b00b7c075
AlyMBarakat <aly.m.barakat@gmail.com>
2021-03-31 02:07:33 +0200

    Create cloud API

---
+++ src/controllers/cloud/videos.ts
@@ -0,0 +1,21 @@
+import Express from "express";
+import { Request, Response } from "express";
+import { getConnection, Repository } from "typeorm";
+
+const router = Express.Router();
+
+
+/*
+
+    Generate URL endpoint
+
+*/
+
+
+/*
+
+    Job completed endpoint
+
+*/
+
+export default router;
