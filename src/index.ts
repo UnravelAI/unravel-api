d9ed6daf09d727c7cf2b28000a3c924d6b5188ce
AlyMBarakat <aly.m.barakat@gmail.com>
2021-03-31 06:08:23 +0200

    Add production database connection options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 import Express from "express";
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions, ConnectionOptions } from "typeorm";
 import cors from "cors";
 
 // import routes
@@ -15,8 +15,40 @@ dotenv.config();
 // as if it were an environment variable
 const port = process.env.SERVER_PORT;
 
+const getOptions = async () => {
+    let connectionOptions: ConnectionOptions;
+    connectionOptions = {
+        type: 'postgres',
+        synchronize: false,
+        logging: false,
+        extra: {
+            ssl: true,
+        },
+        entities: ['dist/entity/*.*'],
+    };
+    if (process.env.DATABASE_URL) {
+        Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+    } else {
+        // gets your default configuration
+        // you could get a specific config by name getConnectionOptions('production')
+        // or getConnectionOptions(process.env.NODE_ENV)
+        connectionOptions = await getConnectionOptions();
+    }
+
+    return connectionOptions;
+};
+
+const connect2Database = async (): Promise<void> => {
+    const typeormconfig = await getOptions();
+    await createConnection(typeormconfig);
+};
+
+connect2Database().then(async () => {
+    console.log('Connected to database');
+});
+
 // create connection using default connection in ormconfig
-createConnection();
+// createConnection();
 
 const App = Express();
 App.use(Express.json());
