38286a0305b1985a08e2eed0f0d089bf88c63149
AlyMBarakat <aly.m.barakat@gmail.com>
2021-04-01 20:18:56 +0200

    Reset to default typeorm configuration

--- src/index.ts
+++ src/index.ts
@@ -15,42 +15,40 @@ dotenv.config();
 // as if it were an environment variable
 const port = process.env.PORT;
 
-// const getOptions = async () => {
-//     let connectionOptions: ConnectionOptions;
-//     connectionOptions = {
-//         type: 'postgres',
-//         synchronize: false,
-//         logging: false,
-//         extra: {
-//             ssl: true,
-//         },
-//         entities: ['dist/entity/*.*'],
-//         migrationsTableName: "UnravelMigrations",
-//         migrations: ["dist/migration/*.js"],
-//         subscribers: ["dist/subscribers/*.js"],
-//         cli: {
-//             entitiesDir: "src/entity",
-//             migrationsDir: "src/migration",
-//             subscribersDir: "src/subscriber"
-//         }
-//     };
-//     if (process.env.DATABASE_URL) {
-//         Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
-//     } else {
-//         // gets your default configuration
-//         // you could get a specific config by name getConnectionOptions('production')
-//         // or getConnectionOptions(process.env.NODE_ENV)
-//         connectionOptions = await getConnectionOptions();
-//     }
+const getOptions = async () => {
+    let connectionOptions: ConnectionOptions;
+    connectionOptions = {
+        type: 'postgres',
+        synchronize: false,
+        logging: false,
+        extra: {
+            ssl: true,
+        },
+        entities: ['dist/entity/*.*'],
+        migrationsTableName: "UnravelMigrations",
+        migrations: ["dist/migration/*.js"],
+        subscribers: ["dist/subscribers/*.js"],
+        cli: {
+            entitiesDir: "src/entity",
+            migrationsDir: "src/migration",
+            subscribersDir: "src/subscriber"
+        }
+    };
+    if (process.env.DATABASE_URL) {
+        Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+    } else {
+        // gets your default configuration
+        // you could get a specific config by name getConnectionOptions('production')
+        // or getConnectionOptions(process.env.NODE_ENV)
+        connectionOptions = await getConnectionOptions();
+    }
 
-//     return connectionOptions;
-// };
+    return connectionOptions;
+};
 
 const connect2Database = async (): Promise<void> => {
-    // const typeormconfig = await getOptions();
-    // await createConnection(typeormconfig);
-    await createConnection();
-
+    const typeormconfig = await getOptions();
+    await createConnection(typeormconfig);
 };
 
 connect2Database().then(async () => {
