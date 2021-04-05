4a835137fef6dd19612b406ef6d5161a7289ff89
Khalid Khalil <khalidkhalil@Khalids-MacBook-Air.local>
2021-04-05 22:22:19 +0200

    Remove unnecessary ':id' query param from generate streamable URL endpoint

--- src/controllers/cloud/videos.ts
+++ src/controllers/cloud/videos.ts
@@ -10,16 +10,16 @@ const router = Express.Router();
     Generate URL endpoint
 
 */
-router.put("/:id/streamablePath", async (req: Request, res: Response) => {
-    try{
+router.put("/generateStreamingURL", async (req: Request, res: Response) => {
+    try {
         const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
-        const videoFileName:string = req.body.fileName;
-        const guid:string = req.body.guid ;
-        const fileNameWExtension=videoFileName.substring(0,videoFileName.lastIndexOf('.'));
-        const streamableURL:string = "https://d10n7efzl01lxo.cloudfront.net/"+guid+"/"+fileNameWExtension+".m3u8";
+        const videoFileName: string = req.body.fileName;
+        const guid: string = req.body.guid;
+        const fileNameWExtension = videoFileName.substring(0, videoFileName.lastIndexOf('.'));
+        const streamableURL: string = "https://d10n7efzl01lxo.cloudfront.net/" + guid + "/" + fileNameWExtension + ".m3u8";
         console.log(streamableURL);
-        const updateResult= await videoRepository.update({fileName:videoFileName},{streamingUrl:streamableURL});
-        if(!updateResult.affected){
+        const updateResult = await videoRepository.update({ fileName: videoFileName }, { streamingUrl: streamableURL });
+        if (!updateResult.affected) {
             return res.status(404).json({
                 message: "Video not found"
             })
@@ -29,7 +29,7 @@ router.put("/:id/streamablePath", async (req: Request, res: Response) => {
             url: streamableURL,
         })
     }
-    catch (error){
+    catch (error) {
         return res.status(500).json({
             message: error.message,
         });
