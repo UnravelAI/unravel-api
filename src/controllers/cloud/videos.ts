5f061bca2b41ff67aa703e5cc9a96d4cfce58b1e
AlyMBarakat <aly.m.barakat@gmail.com>
2021-04-17 22:15:37 +0200

    Add audio url in generateStreamingURL and add google stt transcribe on jobSubmit

--- src/controllers/cloud/videos.ts
+++ src/controllers/cloud/videos.ts
@@ -3,7 +3,8 @@ import { Request, Response } from "express";
 import { getConnection, Repository } from "typeorm";
 import { Video } from "../../entity/video";
 const router = Express.Router();
-
+import axios from "axios";
+import transcribe from "../../helpers/transcribe";
 
 /*
 
@@ -17,7 +18,7 @@ router.put("/generateStreamingURL", async (req: Request, res: Response) => {
         const guid: string = req.body.guid;
         const fileNameWExtension = videoFileName.substring(0, videoFileName.lastIndexOf('.'));
         const streamableURL: string = "https://d10n7efzl01lxo.cloudfront.net/" + guid + "/AppleHLS1/" + fileNameWExtension + ".m3u8";
-        const audioURL: string = "https://d10n7efzl01lxo.cloudfront.net/" + guid + "/FileGroup/" + fileNameWExtension + ".wav";
+        const audioURL: string = "https://d10n7efzl01lxo.cloudfront.net/" + guid + "/FileGroup1/" + fileNameWExtension + ".wav";
         console.log(streamableURL);
         console.log(audioURL);
         const updateResult = await videoRepository.update(
@@ -49,9 +50,35 @@ router.put("/generateStreamingURL", async (req: Request, res: Response) => {
 router.put("/jobStatus", async (req: Request, res: Response) => {
     try {
         const fileName: string = req.body.fileName;
+        const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
+        // retrieve video
+        const video = await videoRepository.findOne({ fileName });
+        console.log(video);
+        if (!video || !video.audioUrl) {
+            return res.status(404).json({
+                message: "Error: video/audio not found",
+            });
+        }
+        const uri = video.audioUrl;
+        console.log("Fetching audio file from: ", uri);
+        const audioFile = await axios.get(uri, { responseType: 'arraybuffer' });
+        const audioBytes: string = audioFile.data.toString('base64');
+        // stt: request body
+        const audio = {
+            content: audioBytes,
+        }
+        // stt: request config
+        const config = {
+            encoding: 'LINEAR16',
+            sampleRateHertz: 16000,
+            languageCode: 'en-US',
+            model: 'video', // For better accuracy specify data model: video, phone-call...
+            enableWordTimeOffsets: true, // allow word specific time offset
+        };
+        console.log("Audio bytes captured...");
+        await transcribe(audio, config);
 
         // update jobStatus for the specified file
-        const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
         const updateResult = await videoRepository.update({ fileName }, { jobCompleted: true });
         console.log(updateResult.affected);
         if (!updateResult.affected) {
@@ -64,6 +91,16 @@ router.put("/jobStatus", async (req: Request, res: Response) => {
         });
 
     } catch (error) {
+        if (error.type === "STT_ERROR") {
+            return res.status(500).json({
+                message: error,
+            });
+        }
+        if (error.response.status === 404) {
+            return res.status(404).json({
+                message: "invalid audio url",
+            });
+        }
         return res.status(500).json({
             message: error.message,
         });
