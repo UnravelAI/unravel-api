875a0deea4a29b8ad0842cd128e429f5afe7d375
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-03 00:04:42 +0200

    Separate file fileUpload in a helper

--- src/controllers/videos.ts
+++ src/controllers/videos.ts
@@ -3,9 +3,7 @@ import { Request, Response } from "express";
 import { getConnection, Repository } from "typeorm";
 import { Video } from "../entity/video";
 import { Material } from "../entity/material";
-import AWS from "aws-sdk";
-import Multer from "multer";
-import MulterS3 from "multer-s3";
+import videoUpload from "../helpers/fileUpload";
 
 const router = Express.Router();
 
@@ -14,25 +12,6 @@ const router = Express.Router();
     Upload video endpoint
 
 */
-
-const s3 = new AWS.S3({
-    region: 'us-east-1',
-});
-
-const videoUpload = Multer({
-    storage: MulterS3({
-        s3,
-        acl: 'public-read',
-        bucket: 'unravel-foundation-source71e471f1-pp7azgexeegs',
-        metadata: (req, file, cb) => {
-            cb(null, { fieldname: file.fieldname });
-        },
-        key: (req, file, cb) => {
-            cb(null, "assets01/" + Date.now().toString() + "-" + file.originalname);
-        }
-    }),
-});
-
 router.post("/material/:id/upload", videoUpload.single("video"), async (req: Request, res: Response) => {
     try {
         const fileName = (req.file as any).key;
@@ -58,18 +37,5 @@ router.post("/material/:id/upload", videoUpload.single("video"), async (req: Req
         });
     }
 });
-
-/*
-
-    Generate URL endpoint
-
-*/
-
-
-/*
-
-    Job completed endpoint
-
-*/
 
 export default router;
