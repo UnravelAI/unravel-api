738b5f0a9c6d6754de2a3bd82b5d2a6f59b180bd
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-03 04:59:04 +0200

    Refactor endpoints and update urls

--- src/controllers/cloud/videos.ts
+++ src/controllers/cloud/videos.ts
@@ -1,10 +1,9 @@
 import Express from "express";
 import { Request, Response } from "express";
 import { getConnection, Repository } from "typeorm";
-import { Video } from "../../entity/video";
+import { Video, videoStatus } from "../../entity/video";
 const router = Express.Router();
 import { transcribe } from "../../helpers/transcribeAWS";
-
 /*
 
     Generate URL endpoint
@@ -18,8 +17,6 @@ router.put("/generateStreamingURL", async (req: Request, res: Response) => {
         const fileNameWExtension = videoFileName.substring(0, videoFileName.lastIndexOf('.'));
         const streamableURL: string = "https://d10n7efzl01lxo.cloudfront.net/" + guid + "/AppleHLS1/" + fileNameWExtension + ".m3u8";
         const audioURL: string = "s3://unravel-foundation-destination920a3c57-lzvld8jwflhj/" + guid + "/FileGroup1/" + fileNameWExtension + "audio.mp3";
-        console.log(streamableURL);
-        console.log(audioURL);
         const updateResult = await videoRepository.update(
             { fileName: videoFileName },
             {
@@ -47,16 +44,15 @@ router.put("/generateStreamingURL", async (req: Request, res: Response) => {
 
 /*
 
-    set video job status and start transcribing
+    start transcribing
 
 */
-router.put("/jobStatus", async (req: Request, res: Response) => {
+router.put("/transcribe", async (req: Request, res: Response) => {
     try {
         const fileName: string = req.body.fileName;
         const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
         // retrieve video
         const video = await videoRepository.findOne({ fileName });
-        console.log(video);
         if (!video || !video.audioUrl) {
             return res.status(404).json({
                 message: "Error: video/audio not found",
@@ -66,15 +62,8 @@ router.put("/jobStatus", async (req: Request, res: Response) => {
         const audioURL = video.audioUrl;
         const transcribeJobName = fileName.substring(0, fileName.lastIndexOf('.')); // fileName without extension
         await transcribe(audioURL, transcribeJobName);
-        // update jobStatus for the specified file
-        const updateResult = await videoRepository.update({ fileName }, { jobCompleted: true });
-        if (!updateResult.affected) {
-            return res.status(404).json({
-                message: "Error: video not found",
-            });
-        }
         return res.status(200).json({
-            message: `jobStatus updated successfully for video: ${fileName}`,
+            message: `Starting transcription job for: ${fileName}`,
         });
 
     } catch (error) {
@@ -96,19 +85,19 @@ router.put("/jobStatus", async (req: Request, res: Response) => {
 
 /*
 
-    set video job status and start transcribing
+    set status to editable
 
 */
-router.put("/jobStatus/transcribe", async (req: Request, res: Response) => {
+router.put("/status/editable", async (req: Request, res: Response) => {
     try {
         const transcribeJobName: string = req.body.transcribeJobName;
         const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
         const fileNameWithoutEx = transcribeJobName.substring(0, transcribeJobName.lastIndexOf('.')); // fileName without extension
         const transcriptionUrl: string = "https://d10n7efzl01lxo.cloudfront.net/" + transcribeJobName;
-        // update transcribing status jobStatus for the specified file
+        // update video status to editable
         const updateResult = await videoRepository.update({ fileName: `${fileNameWithoutEx}.mp4` }, {
-            transcriptionCompleted: true,
             transcriptionUrl,
+            status: videoStatus.EDITABLE,
         });
         if (!updateResult.affected) {
             return res.status(404).json({
@@ -116,7 +105,7 @@ router.put("/jobStatus/transcribe", async (req: Request, res: Response) => {
             });
         }
         return res.status(200).json({
-            message: `Transcribe job status updated successfully for video: ${fileNameWithoutEx}`,
+            message: `Video status updated successfully for video: ${fileNameWithoutEx}`,
         });
 
     } catch (error) {
