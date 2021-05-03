8fbb8336412eabf3a0521ec9075aef43c3e0222c
AlyMBarakat <aly.m.barakat@gmail.com>
2021-05-03 05:56:05 +0200

    Create video editing endpoint

--- src/controllers/cloud/videos.ts
+++ src/controllers/cloud/videos.ts
@@ -63,7 +63,7 @@ router.put("/transcribe", async (req: Request, res: Response) => {
         const transcribeJobName = fileName.substring(0, fileName.lastIndexOf('.')); // fileName without extension
         await transcribe(audioURL, transcribeJobName);
         return res.status(200).json({
-            message: `Starting transcription job for: ${fileName}`,
+            message: `Transcription started for: ${fileName}`,
         });
 
     } catch (error) {
@@ -77,6 +77,48 @@ router.put("/transcribe", async (req: Request, res: Response) => {
                 message: "invalid audio url",
             });
         }
+        return res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
+/*
+
+    start editing -> update status: editing
+
+*/
+router.put("/edit", async (req: Request, res: Response) => {
+    try {
+        const fileName: string = req.body.fileName;
+        const videoRepository: Repository<Video> = await getConnection().getRepository(Video);
+        // retrieve video
+        const video = await videoRepository.findOne({ fileName });
+        if (!video) {
+            return res.status(404).json({
+                message: "Error: video not found",
+            });
+        }
+        if (video.status !== videoStatus.EDITABLE) {
+            return res.status(400).json({
+                message: "Error: video not editable",
+            });
+        }
+        /**
+         *
+         *
+         * Invoke vide editing lambda
+         *
+         *
+         */
+        // update video status to editing
+        const updateResult = await videoRepository.update({ fileName }, {
+            status: videoStatus.EDITING,
+        });
+        return res.status(200).json({
+            message: `Editing started for: ${fileName}`,
+        });
+    } catch (error) {
         return res.status(500).json({
             message: error.message,
         });
